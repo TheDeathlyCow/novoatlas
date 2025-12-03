ac597792d188ec5120643e98564adb4d7bb5ac4e
squoshi <95097230+squoshi@users.noreply.github.com>
2025-12-03 01:04:46 -0600

    Fix Forge Crash 1.20.1 (#13)
    * Port to 1.20.1 Forge/Fabric
    
    Also disabled the TerraBlender mixin because it most likely isn't necessary on this version (tell me if I'm wrong though)
    
    * Formatted
    
    * cleanup
    
    * Fix mixins on Forge
    
    * Update, add publishing, fix TerraBlender mixin
    
    * Fix publishing
    
    * patch
    
    * fix forge platform module

--- forge/src/main/java/com/thedeathlycow/novoatlas/forge/NovoAtlasPlatformImpl.java
+++ forge/src/main/java/com/thedeathlycow/novoatlas/forge/NovoAtlasPlatformImpl.java
@@ -1,9 +1,9 @@
 package com.thedeathlycow.novoatlas.forge;
 
-import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.loading.LoadingModList;
 
 public class NovoAtlasPlatformImpl {
     public static boolean isModLoaded(String modid) {
-        return ModList.get().isLoaded(modid);
+        return LoadingModList.get().getModFileById(modid) != null;
     }
 }
