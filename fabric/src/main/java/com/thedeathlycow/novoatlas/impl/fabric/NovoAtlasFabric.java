9203f70ffeeac74ea2786d3eb706655c603ff871
TheDeathlyCow <53499406+TheDeathlyCow@users.noreply.github.com>
2026-09-03 00:59:48 -1000

    Optimize biome maps (#22)
    * implement support for reduced size biome maps
    
    * downscale example datapack images
    
    * replace use_reduced_size_biome_maps with a new biome source type
    
    * update example datapacks
    
    * cleanup

--- fabric/src/main/java/com/thedeathlycow/novoatlas/impl/fabric/NovoAtlasFabric.java
+++ fabric/src/main/java/com/thedeathlycow/novoatlas/impl/fabric/NovoAtlasFabric.java
@@ -2,15 +2,16 @@ package com.thedeathlycow.novoatlas.impl.fabric;
 
 import com.thedeathlycow.novoatlas.impl.NovoAtlas;
 import com.thedeathlycow.novoatlas.impl.gen.*;
-import com.thedeathlycow.novoatlas.impl.platform.Services;
-import com.thedeathlycow.novoatlas.impl.registry.ImageManager;
-import com.thedeathlycow.novoatlas.impl.registry.NovoAtlasBuiltinRegistries;
-import com.thedeathlycow.novoatlas.impl.registry.NovoAtlasRegistries;
+import com.thedeathlycow.novoatlas.impl.gen.biome.BiomeCellColorMapBiomeSource;
 import com.thedeathlycow.novoatlas.impl.gen.biome.ColorMapBiomeSource;
 import com.thedeathlycow.novoatlas.impl.gen.interpolation.Bicubic;
 import com.thedeathlycow.novoatlas.impl.gen.interpolation.Bilinear;
 import com.thedeathlycow.novoatlas.impl.gen.interpolation.Lanczos;
 import com.thedeathlycow.novoatlas.impl.gen.interpolation.NearestNeighbour;
+import com.thedeathlycow.novoatlas.impl.platform.Services;
+import com.thedeathlycow.novoatlas.impl.registry.ImageManager;
+import com.thedeathlycow.novoatlas.impl.registry.NovoAtlasBuiltinRegistries;
+import com.thedeathlycow.novoatlas.impl.registry.NovoAtlasRegistries;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.registry.DynamicRegistries;
 import net.fabricmc.fabric.api.resource.v1.ResourceLoader;
@@ -30,10 +31,14 @@ public final class NovoAtlasFabric implements ModInitializer {
         DynamicRegistries.register(NovoAtlasRegistries.MAP_INFO, MapInfo.DIRECT_CODEC);
 
         Registry.register(BuiltInRegistries.CHUNK_GENERATOR, NovoAtlas.id("image_map"), ImageMapChunkGenerator.CODEC);
+
         Registry.register(BuiltInRegistries.BIOME_SOURCE, NovoAtlas.id("color_map"), ColorMapBiomeSource.CODEC);
+        Registry.register(BuiltInRegistries.BIOME_SOURCE, NovoAtlas.id("biome_cell_color_map"), BiomeCellColorMapBiomeSource.CODEC);
+
         Registry.register(BuiltInRegistries.DENSITY_FUNCTION_TYPE, NovoAtlas.id("heightmap"), HeightmapDensityFunction.DATA_CODEC);
         Registry.register(BuiltInRegistries.DENSITY_FUNCTION_TYPE, NovoAtlas.id("get_height_from_map"), GetHeightFromMapDensityFunction.DATA_CODEC);
         Registry.register(BuiltInRegistries.DENSITY_FUNCTION_TYPE, NovoAtlas.id("get_preliminary_height_from_map"), GetPreliminaryHeightFromMapDensityFunction.DATA_CODEC);
+
         Registry.register(NovoAtlasBuiltinRegistries.INTERPOLATOR_TYPE, NovoAtlas.id("nearest_neighbor"), NearestNeighbour.CODEC);
         Registry.register(NovoAtlasBuiltinRegistries.INTERPOLATOR_TYPE, NovoAtlas.id("bilinear"), Bilinear.CODEC);
         Registry.register(NovoAtlasBuiltinRegistries.INTERPOLATOR_TYPE, NovoAtlas.id("bicubic"), Bicubic.CODEC);
