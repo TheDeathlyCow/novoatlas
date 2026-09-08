cf949f12f7a3f9a7c4c1701eea0b9079a3da3b84
TheDeathlyCow <53499406+TheDeathlyCow@users.noreply.github.com>
2026-09-07 19:18:21 -1000

    Add image wrapping (#26)
    * add edge handling
    
    * add default edge handling to example packs
    
    * fix fixedvalue edge handling using the same value for the entire world
    
    * convert to enum and remove fixed value wrapping mode
    
    * remove doFill implementation as it is no longer necessary
    
    * fix wrong image wrapping in example pack

--- common/src/main/java/com/thedeathlycow/novoatlas/impl/image/interpolation/Bilinear.java
+++ common/src/main/java/com/thedeathlycow/novoatlas/impl/image/interpolation/Bilinear.java
@@ -2,13 +2,14 @@ package com.thedeathlycow.novoatlas.impl.image.interpolation;
 
 import com.mojang.serialization.MapCodec;
 import com.thedeathlycow.novoatlas.impl.image.MapImage;
+import com.thedeathlycow.novoatlas.impl.image.MapInfo;
 import net.minecraft.util.Mth;
 
 public final class Bilinear implements Interpolator {
     public static final MapCodec<Bilinear> CODEC = MapCodec.unit(new Bilinear());
 
     @Override
-    public double sample(double x, double z, MapImage image) {
+    public double sample(double x, double z, MapImage image, MapInfo mapInfo) {
         // x and z are the truncated (floor) coordinates
         // deltaX and deltaZ are the fractional parts
         int truncatedX = Mth.floor(x);
@@ -21,10 +22,10 @@ public final class Bilinear implements Interpolator {
         int nextX = Math.min(truncatedX + 1, image.width() - 1);
         int nextZ = Math.min(truncatedZ + 1, image.height() - 1);
 
-        int topLeft = image.getPixelValue(truncatedX, truncatedZ);
-        int topRight = image.getPixelValue(nextX, truncatedZ);
-        int bottomLeft = image.getPixelValue(truncatedX, nextZ);
-        int bottomRight = image.getPixelValue(nextX, nextZ);
+        int topLeft = image.getPixelValue(truncatedX, truncatedZ, mapInfo.imageWrapping());
+        int topRight = image.getPixelValue(nextX, truncatedZ, mapInfo.imageWrapping());
+        int bottomLeft = image.getPixelValue(truncatedX, nextZ, mapInfo.imageWrapping());
+        int bottomRight = image.getPixelValue(nextX, nextZ, mapInfo.imageWrapping());
 
         return Mth.lerp2(deltaX, deltaZ, topLeft, topRight, bottomLeft, bottomRight);
     }
--- common/src/main/java/com/thedeathlycow/novoatlas/impl/image/interpolation/NearestNeighbour.java
+++ common/src/main/java/com/thedeathlycow/novoatlas/impl/image/interpolation/NearestNeighbour.java
@@ -2,13 +2,14 @@ package com.thedeathlycow.novoatlas.impl.image.interpolation;
 
 import com.mojang.serialization.MapCodec;
 import com.thedeathlycow.novoatlas.impl.image.MapImage;
+import com.thedeathlycow.novoatlas.impl.image.MapInfo;
 
 public final class NearestNeighbour implements Interpolator {
     public static final MapCodec<NearestNeighbour> CODEC = MapCodec.unit(new NearestNeighbour());
 
     @Override
-    public double sample(double x, double z, MapImage image) {
-        return image.getTruncated(x, z);
+    public double sample(double x, double z, MapImage image, MapInfo mapInfo) {
+        return image.getTruncated(x, z, mapInfo.imageWrapping());
     }
 
     @Override
